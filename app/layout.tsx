3fe87d6f7dbea0de16efeb4ae3aa7f397b2398fe
MaxBrych <max.brych03@gmail.com>
2023-05-29 17:03:14 +0200

    removed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./globals.css";
 import { Inter, Space_Grotesk } from "next/font/google";
@@ -5,11 +6,6 @@ import { CacheProvider } from "@chakra-ui/next-js";
 
 const inter = Inter({ subsets: ["latin"] });
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "The Netizen",
-  description: "A collection of links to helpful resources about The Netizen. ",
-};
 
 export default function RootLayout({
   children,
